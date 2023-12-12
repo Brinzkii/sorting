39e43e230f15adc7b9c7e2e2fa6a828610e2addb
Brinzkii <dmbezehertny@gmail.com>
2023-12-12 15:46:03 -0500

    complete insertion and selection sort

--- selection.js
+++ selection.js
@@ -1,3 +1,16 @@
-function selectionSort() {}
+function selectionSort(arr) {
+	for (let i = 0; i < arr.length - 1; i++) {
+		let min_idx = i;
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[min_idx]) {
+				min_idx = j;
+			}
+		}
+		let temp = arr[min_idx];
+		arr[min_idx] = arr[i];
+		arr[i] = temp;
+	}
+	return arr;
+}
 
 module.exports = selectionSort;
